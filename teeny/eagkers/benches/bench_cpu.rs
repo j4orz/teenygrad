32b0ccfff629c2b8258819e4b849b7dd28fc9d40
jeffzh4ng <zhang.jeffreyd@gmail.com>
2026-04-19 13:51:44 -0400

    eagkers: remove bencher bench

--- teeny/eagkers/benches/bench_cpu.rs
+++ teeny/eagkers/benches/bench_cpu.rs
@@ -1,41 +1,45 @@
-#![feature(test)]
-extern crate test;
+use divan::counter::ItemsCount;
+use eagkers::cpu::{Layout, Transpose};
 
-#[cfg(test)]
-#[rustfmt::skip]
-mod benches {
-  use test::Bencher;
-  use eagkers::cpu::{Layout, Transpose};
+fn main() { divan::main(); }
 
-  fn inputs(size: usize) -> (Vec<f32>, Vec<f32>, Vec<f32>) {
-    (vec![1.0f32; size * size], vec![1.0f32; size * size], vec![0.0f32; size * size])
-  }
+fn inputs(n: usize) -> (Vec<f32>, Vec<f32>, Vec<f32>) {
+  (vec![1.0f32; n * n], vec![1.0f32; n * n], vec![0.0f32; n * n])
+}
 
-  fn bench_eagkers_gemm(bench: &mut Bencher, size: usize) {
-    let (a, bm, mut c) = inputs(size);
-    let n = size as i32;
-    bench.iter(||
+#[divan::bench_group]
+mod gemm {
+  use super::*;
+
+  #[divan::bench(args = [16, 32, 64, 128, 256, 512, 1024])] //, 2048, 4096, 8192])]
+  fn eagkers(b: divan::Bencher, n: usize) {
+    let (a, bm, mut c) = inputs(n);
+    b.counter(ItemsCount::new(2 * n * n * n))
+     .bench_local(||
       eagkers::cpu::sgemm(
-        Layout::RowMajor, Transpose::None, Transpose::None,
-        n, n, n, 1.0, &a, n, &bm, n, 0.0, &mut c, n));
-  }
-
-  #[cfg(feature = "cpudev")]
-  fn bench_cpudev_gemm(bench: &mut Bencher, size: usize) {
-    let (a, bm, mut c) = inputs(size);
-    bench.iter(|| { unsafe {
-      cblas::sgemm(
-        cblas::Layout::RowMajor,
-        cblas::Transpose::None, cblas::Transpose::None,
-        size as i32, size as i32, size as i32,
-        1.0, &a, size as i32, &bm, size as i32,
-        0.0, &mut c, size as i32,
+        Layout::RowMajor, 
+        Transpose::None, 
+        Transpose::None, 
+        n as i32, n as i32, n as i32, 
+        1.0, &a, n as i32, &bm, n as i32, 
+        0.0, &mut c, n as i32
       )
-    }});
+    );
   }
 
-  #[bench] fn bench_eagkers_gemm_256(b: &mut Bencher) { bench_eagkers_gemm(b, 256) }
-
   #[cfg(feature = "cpudev")]
-  #[bench] fn bench_cpudev_gemm_256(b: &mut Bencher) { bench_cpudev_gemm(b, 256) }
+  #[divan::bench(args = [16, 32, 64, 128, 256, 512, 1024])] //, 2048, 4096, 8192])]
+  fn cblas(b: divan::Bencher, n: usize) {
+    let (a, bm, mut c) = inputs(n);
+    b.counter(ItemsCount::new(2 * n * n * n))
+     .bench_local(|| unsafe {
+       cblas::sgemm(
+         cblas::Layout::RowMajor,
+         cblas::Transpose::None, cblas::Transpose::None,
+         n as i32, n as i32, n as i32,
+         1.0, &a, n as i32, &bm, n as i32,
+         0.0, &mut c, n as i32,
+       );
+     });
+  }
 }
