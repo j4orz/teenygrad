3b95c4073dc8dd256092553c2d0a86b995fd1e20
jeffzh4ng <zhang.jeffreyd@gmail.com>
2026-04-29 09:13:02 -0400

    evaluate makemore bigram on test (reusing train)

--- teeny/examples/4.1-makemore-ngram-np.py
+++ teeny/examples/4.1-makemore-ngram-np.py
@@ -41,58 +41,87 @@ for di in dataset:
   for x_char,y_char in zip(di_normalized, di_normalized[1:]): # here we
     conditional_counts_dict[(x_char,y_char)] = conditional_counts_dict.get((x_char,y_char), 0) + 1
 sorted_conditional_counts_dict = sorted(conditional_counts_dict.items(), key = lambda x: -x[1])
-print("2D (char,char) histogram using python's dict:\n", sorted_conditional_counts_dict)
+# print("2D (char,char) histogram using python's dict:\n", sorted_conditional_counts_dict)
 
 
 
 
-
+# --- "TRAINING" ---
+print("--- TRAINING ---")
 # We will now construct the same 2d histogram, but with numpy's ndarray instead of python's dict
 import numpy as np # we use numpy rather than torch to emphasize that the bigram model does NOT required any deep learning functionality
 vocab = sorted(list(set(''.join(dataset)))) # construct vocab
 c2i = {c:i+1 for i,c in enumerate(vocab)}   # construct map<char,ord>
 c2i['.'] = 0                                # with . as the start token and end token, to remove counting freq of (<E>*) and (*<S>) which are all 0
 V = len(c2i)                                # evaluate the vocab len V
-D_VV = np.zeros((V,V), dtype=np.int32)      # and use V to construct D_VV
+C_VV = np.zeros((V,V), dtype=np.int32)      # and use V to construct D_VV
 
 for di in dataset:
   di_normalized = ['.'] + list(di) + ['.']
   for x_char,y_char in zip(di_normalized, di_normalized[1:]):
     x_index, y_index = c2i[x_char], c2i[y_char] # use map<char, ord> to lookup the coordinate index needed for D_VV
-    D_VV[x_index, y_index] += 1                 # update D_VV
+    C_VV[x_index, y_index] += 1                 # update D_VV
 
+# normalize counts C_VV to probs P_VV
+C_VVf32 = (C_VV+1).astype(np.float32)    # inductive bias (locally smooth)
+s_V1 = C_VVf32.sum(axis=1,keepdims=True) # reduce along axis=1 because we want p(y|x) not p(x|y)
+P_VV = C_VVf32 / s_V1                    # (V, V) / (V, 1) broadcasts
+
+# because the data matrix D_VV is 2 dimensional, we can print it.
+# the way to semantically interpret the table is that is to first understand which axis of the ndarray represents which semantic information
+# for D_VV, the elements are the counts of bigrams (c1,c2) which is acessed by ord(c1) with axis=0 and ord(c2) with axis=2
+# now, since numpy's ndarray's are row major order, axis=0 gets printed vertically from up to down while axis=1 gets printed horizontally from left to right  
 i2c = {i:c for c,i in c2i.items()}  # invert map<char, ord> to map<ord, char> because looping with enumerate provides access to indices
 header = '    ' + ' '.join(f'{i2c[y_index]:>4}' for y_index in range(V))
-
-# because the data matrix D_VV is 2 dimensional, we can print it. the way to semantically interpret the table is that
-# the numbers are counts of bigrams (character pairs) of the characters on the vertical (x) axis *followed by* the chracters on the horizontal (y) axis
-
-# (since numpy.ndarray's are row major order, the first dimension is printed from up to down)
-# (this might be somewhat confusing if you have the xy-cartesian plane in mind)
-# (if it helps, replace x_index with row_index and y_index with col_index)
 print("2D (ord, ord) histogram using numpy ndarray")
 print(header)
-for x_index, row in enumerate(D_VV):
+for x_index, row in enumerate(C_VV+1):
   x_char = f'{i2c[x_index]:>4}'
   print(x_char, ' '.join(f'{count:>4}' for count in row))
 
 
-# inference loop
+
+# --- INFERENCE ---
+print("\n\n--- INFERENCE ---")
 rng = np.random.default_rng(1337)
-sample_count = 50
+sample_count = 10
 
 for _ in range(sample_count):
   output, sample_index = [], 0
   while True:
     # 1. get p(Y|X)
-    counts = D_VV[sample_index].astype(np.float32)
-    pYcondX = (counts / counts.sum()).squeeze()
+    pYcondX_V = P_VV[sample_index].squeeze()
 
     # 2. sample p(Y|X)
-    sample_index = rng.choice(len(pYcondX), size=1, replace=True, p=pYcondX)
+    sample_index = rng.choice(len(pYcondX_V), size=1, replace=True, p=pYcondX_V)
     sample_char = i2c[sample_index.item()]
     output.append(sample_char)
 
     # 3. if sampled end token, break
     if sample_index == 0: break
   print(''.join(output))
+
+
+# --- TEST ---
+print("\n\n--- TEST ---")
+loglikelihooddataset,n = 0.0, 0
+for di in dataset:
+  di_normalized = ['.'] + list(di) + ['.']
+  for x_char,y_char in zip(di_normalized, di_normalized[1:]):
+    x_index, y_index = c2i[x_char], c2i[y_char] # use map<char, ord> to lookup the coordinate index needed for D_VV
+    pycondx = P_VV[x_index, y_index]
+    logpycondx = np.log(pycondx)
+
+    loglikelihooddataset += logpycondx
+    n += 1
+    # print(f'{x_char}{y_char}: {pycondx:.4f} {logpycondx:.4f}')
+
+# maximize likelihood
+# maximize loglikelihood
+# minimize -loglikelihood
+# minimize -1/n loglikelihood
+nlldataset = -loglikelihooddataset
+avgnlldataset = nlldataset / n
+print(f'{loglikelihooddataset=}')
+print(f'{nlldataset=}')
+print(f'{avgnlldataset=}')
