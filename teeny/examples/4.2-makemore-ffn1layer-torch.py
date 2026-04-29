eb145ccd3c9ea293ada20c5c2dd8414d7f5b33ce
jeffzh4ng <zhang.jeffreyd@gmail.com>
2026-04-29 13:25:42 -0400

    examples: implement training loop for single layer makemore net

--- teeny/examples/4.2-makemore-ffn1layer-torch.py
+++ teeny/examples/4.2-makemore-ffn1layer-torch.py
@@ -10,105 +10,95 @@
 # evaluating neural networks and (2) the overall framework of language modeling that includes model training,
 # sampling, and the evaluation of a loss (e.g. the negative log likelihood for classification).
 
-print("--- DATA ---")
+print("\n\n--- DATA ---")
 import torch
 import torch.nn.functional as F
 dataset = open('./examples/data/names.txt', 'r').read().splitlines()
-N = len(dataset)
+D = len(dataset)                            # D is the length of the dataset. N is reserved for the number of examples, in which each di can comprise many of
 vocab = sorted(list(set(''.join(dataset)))) # construct vocab
-c2i = {c:i+1 for i,c in enumerate(vocab)}   # construct map<char,ord>
+c2i = {c:i+1 for i,c in enumerate(vocab)}   # construct map<char,usize>
 c2i['.'] = 0                                # with . as the start token and end token, to remove counting freq of (<E>*) and (*<S>) which are all 0
 V = len(c2i)                                # evaluate the vocab len V
 
-xsordpy, ysordpy = [], []
-for di in dataset[:1]:
-  di_normalized = ['.'] + list(di) + ['.']                              # normalize each word di
-  for x_char,y_char in zip(di_normalized, di_normalized[1:]):           # loop through the (x,y) "self-supervised" bigrams of each word di with zip
-    x_index, y_index = c2i[x_char], c2i[y_char]                         # use map<char, ord> to map representation from discrete characters to discrete integers (ords)
-    xsordpy.append(x_index), ysordpy.append(y_index)                    # append
+xindicesraw, yindicesraw = [], []
+for di in dataset:                                                 # change to dataset[:1] when debugging to limit the number of N examples
+  di_normalized = ['.'] + list(di) + ['.']                         # normalize each word di
+  for x_char,y_char in zip(di_normalized, di_normalized[1:]):      # loop through the (x,y) "self-supervised" bigrams of each word di with zip
+    x_index, y_index = c2i[x_char], c2i[y_char]                    # use map<char, usize> to map representation from discrete characters to discrete integers (ords)
+    xindicesraw.append(x_index), yindicesraw.append(y_index)       # append
 
-xsordtorch, ysordtorch = torch.tensor(xsordpy), torch.tensor(ysordpy)   # then, convert python lists into torch tensors
-print(f'inputs (ord): {xsordtorch}')
-print(f'outputs (ord): {ysordtorch}')
+N = len(xindicesraw)
+xindices_N, yindices_N = torch.tensor(xindicesraw), torch.tensor(yindicesraw)   # then, convert python lists into torch tensors
+print(f'inputs (usize): {xindices_N}'); print(f'outputs (usize): {yindices_N}')
 
-                                                                        # finally, map representation once again from discrete integers to continuous (but local) one hot vectors
-xshot_NV, yshot_NV = F.one_hot(xsordtorch,num_classes=V).float(), F.one_hot(ysordtorch,num_classes=V).float()
-print(f'inputs (1hot): {xshot_NV.shape} {xshot_NV}')
-print(f'outputs (1hot): {xshot_NV.shape} {xshot_NV}')
-
-
-
-
-
-# --- "ARCH" ---
-print("--- ARCH ---")
-g = torch.Generator().manual_seed(1337+1)                        # avgnll: 3.5 -> 4.9
-W_VV = torch.randn((V, V), generator=g)                          # V neurons
-logits_NV = xshot_NV @ W_VV                                      # batch matmul
-counts_NV = logits_NV.exp()                                      # equivalent to C_VV
-probsycondx_NV = counts_NV / counts_NV.sum(dim=1, keepdims=True) # normalize, completing the evaluation of softmax
-# print(f'logits_NV: {logits_NV.shape}, {logits_NV}')
-# print(f'counts_NV: {counts_NV.shape}, {counts_NV}')
-print(f'probs_NV: {probsycondx_NV.shape}, {probsycondx_NV}')
-
-print("--- ARCH UNTRAINED ---")
-i2c = {i:c for c,i in c2i.items()}                               # invert map<char, ord> to map<ord, char> for decoding
-
-# logpD,n = 0.0, 0
-nlls = torch.zeros(5)                                            # replace sum and count tracking for average with .mean() (we won't be pushing logpycondx though only -logpycondx)
-for i in range(5):                                               # loop through the 5 input-outputs (x^(i), y^(i)) of the first word di in dataset
-  xord, yord = xsordtorch[i].item(), ysordtorch[i].item()        # map (x^(i), y^(i))'s index to ordinal
-  xchar, ychar = i2c[xord], i2c[yord]                            # map (x^(i), y^(i))'s ordinal to char
-
-  pYcondx_V = probsycondx_NV[i]
-  pycondx = pYcondx_V[yord]
-  logpycondx = torch.log(pycondx)
-  nll = -logpycondx
-  print(f'(x(^{i}),y(^{i})): ({xchar},{ychar}) ---> py(^{i})condx(^{i})hat: {pycondx:.4f}, logpy(^{i})condx(^{i}): {logpycondx:.4f}, nll: {nll:.4f}')
-
-  nlls[i] = nll
-  # logpD += logpycondx
-  # n += 1
-
-nllD = nlls.sum()
-avgnllD = nlls.mean()
-print(f'{nllD=}, {avgnllD=}')
+                                                                   # finally, map representation once again from discrete integers to continuous (but local) one hot vectors
+x1hots_NV, y1hots_NV = F.one_hot(xindices_N,num_classes=V).float(), F.one_hot(yindices_N,num_classes=V).float()
+print(f'inputs (1hot): {x1hots_NV.shape} {x1hots_NV}'); print(f'outputs (1hot): {x1hots_NV.shape} {x1hots_NV}')
 
 
 
 
 
-# --- "TRAINING" ---
-print("--- TRAINING ---")
+print("\n\n--- ARCH ---")
+g = torch.Generator().manual_seed(1337+1)                          # avgnll: 3.5 -> 4.9
+W_VV = torch.randn((V, V), generator=g, requires_grad=True)        # V neurons
+
+print("\n\n--- TRAINING ---")
+K = 100
+lr = 50
+print(f'{D=}, {N=}, {K=}')
+for k in range(K): # gradient descent
+  # TRAINING FORWARD {k} --- (including softmax) with N examples from dataset D')
+  logits_NV = x1hots_NV @ W_VV                                     # batch matmul print(f'logits_NV: {logits_NV.shape}, {logits_NV}');
+  counts_NV = logits_NV.exp()                                      # equivalent to C_VV print(f'counts_NV: {counts_NV.shape}, {counts_NV}')
+  probsycondx_NV = counts_NV / counts_NV.sum(dim=1, keepdims=True) # normalize, completing the evaluation of softmax
+  # print(f'(forward) probsycondx_NV: {probsycondx_NV.shape}\n {probsycondx_NV}')
+
+                                                                   # vectorized evaluation of loss in TEST section below
+  indices_N = torch.arange(N)                                      # in order to evaluate loss of first N examples, use torch.arange(N) NOT xindices_N
+  loss = -probsycondx_NV[indices_N, yindices_N].log().mean()       # however we do use yindices_N to find the corresponding likelihood predictions of the *targets*
+  print(f'{k=}, {loss=}')                                          # pluck the likelihoods, then eval the log, average it, and take the inverse
+                                                                   # ...to get negative log likelihood
+
+  # TRAINING BACKWARD {k} --- (including softmax) with N examples from dataset D')
+  W_VV.grad = None                                                 # same as zeroing gradients
+  loss.backward()                                                  # eval f'(x)
+  # print(f'(backward): {W_VV.grad.shape}, {W_VV.grad}')
+
+  # TRAINING STEP {k} ---\n')
+  W_VV.data += -lr*W_VV.grad
 
 
 
 
 
-# --- INFERENCE ---
+
+
 print("\n\n--- INFERENCE ---")
 
 
-# --- TEST ---
+
+
+
 print("\n\n--- TEST ---")
-# loglikelihooddataset,n = 0.0, 0
-# for di in dataset[:3]:
-#   di_normalized = ['.'] + list(di) + ['.']
-#   for x_char,y_char in zip(di_normalized, di_normalized[1:]):
-#     x_index, y_index = c2i[x_char], c2i[y_char] # use map<char, ord> to lookup the coordinate index needed for D_VV
-#     pycondx = P_VV[x_index, y_index]
-#     logpycondx = torch.log(pycondx)
+i2c = {i:c for c,i in c2i.items()}                               # invert map<char, ord> to map<ord, char> for decoding
 
-#     loglikelihooddataset += logpycondx
-#     n += 1
-#     # print(f'{x_char}{y_char}: {pycondx:.4f} {logpycondx:.4f}')
+# logpD,n = 0.0, 0
+# nlls = torch.zeros(N)                                            # replace sum and count tracking for average with .mean() (we won't be pushing logpycondx though only -logpycondx)
+# for i in range(N):                                               # loop through the 5 input-outputs (x^(i), y^(i)) of the first word di in dataset
+#   xord, yord = xindices_N[i].item(), yindices_N[i].item()        # map (x^(i), y^(i))'s index to ordinal
+#   xchar, ychar = i2c[xord], i2c[yord]                            # map (x^(i), y^(i))'s ordinal to char
 
-# # maximize likelihood
-# # maximize loglikelihood
-# # minimize -loglikelihood
-# # minimize -1/n loglikelihood
-# nlldataset = -loglikelihooddataset
-# avgnlldataset = nlldataset / n
-# print(f'{loglikelihooddataset=}')
-# print(f'{nlldataset=}')
-# print(f'{avgnlldataset=}')
+#   pYcondx_V = probsycondx_NV[i]
+#   pycondx_ = pYcondx_V[yord]
+#   logpycondx_ = torch.log(pycondx_)
+#   nll = -logpycondx_
+#   print(f'(x(^{i}),y(^{i})): ({xchar},{ychar}) ---> py(^{i})condx(^{i})hat: {pycondx_:.4f}, logpy(^{i})condx(^{i}): {logpycondx_:.4f}, nll: {nll:.4f}')
+
+#   nlls[i] = nll
+  # logpD += logpycondx
+  # n += 1
+
+# nllD = nlls.sum()
+# avgnllD = nlls.mean()
+# print(f'{nllD=}, {avgnllD=}')
